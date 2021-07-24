2b5aae88294a3ef091a3abf68389b0b03c1d0c35
abekoh <abekoh.bcky@gmail.com>
2021-07-25 03:46:10 +0900

    update script

--- scripts/setup-minecraft-server.sh
+++ scripts/setup-minecraft-server.sh
@@ -9,9 +9,5 @@ mkdir -p /mnt/disks/game-data
 mount -o discard,defaults UUID=bc7783d6-280f-4214-930d-3d8a172fccbc /mnt/disks/game-data
 chmod a+w /mnt/disks/game-data
 
-# make symbolic link
-ln -sf /mnt/disks/game-data/minecraft-java ~/minecraft-java
-
 # run minecraft
-RUN_COMMAND="java -Xmx2048M -Xms2048M -jar ~/minecraft-java/server.jar nogui"
-tmux new-session -d -s my_session $RUN_COMMAND
+nohup java -Xmx2048M -Xms2048M -jar /mnt/disks/game-data/minecraft-java/server.jar nogui &
