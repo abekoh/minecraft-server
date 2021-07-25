a1ee6cec2472637b3fb8b6c2ae5e068487daaf07
abekoh <abekoh.bcky@gmail.com>
2021-07-25 16:58:45 +0900

    divide files

---
+++ infrastructure/networks.tf
@@ -0,0 +1,37 @@
+resource "google_compute_network" "vpc_network" {
+  name = "minecraft-network"
+}
+
+resource "google_compute_address" "static_ip" {
+  name = "minecraft-server-static-ip"
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  priority = 9000
+}
+
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "allow-icmp"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "icmp"
+  }
+  priority = 8000
+}
+
+resource "google_compute_firewall" "allow_minecraft" {
+  name    = "allow-minecraft"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["25565"]
+  }
+  target_tags = ["minecraft-server"]
+  priority    = 1000
+}
