57bbd76e3b4c044ef74bcc5be125b8cf8422c8e6
abekoh <abekoh.bcky@gmail.com>
2021-07-31 17:23:02 +0900

    update scripts

--- scripts/setup-discord-bot-server.sh
+++ scripts/setup-discord-bot-server.sh
@@ -4,7 +4,9 @@
 wget https://golang.org/dl/go1.16.6.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.6.linux-amd64.tar.gz
 
+mkdir -p $HOME/src
 export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/src
 
 go get github.com/abekoh/minecraft-server/bot
 
