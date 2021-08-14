f764e816af3afe9aa3469e9fe2fad13f01107e57
abekoh <abekoh.bcky@gmail.com>
2021-08-14 14:20:27 +0900

    format

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "backup" {
-  name          = "minecraft-abekoh-backup"
-  location      = "US-CENTRAL1"
-  storage_class = "REGIONAL"
+  name                        = "minecraft-abekoh-backup"
+  location                    = "US-CENTRAL1"
+  storage_class               = "REGIONAL"
   uniform_bucket_level_access = true
 
   lifecycle_rule {
