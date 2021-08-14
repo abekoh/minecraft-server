cd1e9da7cb5a6f07786aaca343e840c53347f108
abekoh <abekoh.bcky@gmail.com>
2021-08-14 14:19:04 +0900

    set uniform_bucket_level_access

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -2,6 +2,8 @@ resource "google_storage_bucket" "backup" {
   name          = "minecraft-abekoh-backup"
   location      = "US-CENTRAL1"
   storage_class = "REGIONAL"
+  uniform_bucket_level_access = true
+
   lifecycle_rule {
     condition {
       age = 30
