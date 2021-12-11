8e149f9d38bb953fc482637657c242496d6c4738
abekoh <abekoh.bcky@gmail.com>
2021-12-11 14:53:10 +0900

    exclude dynmap

--- scripts/backup-spigot.sh
+++ scripts/backup-spigot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 cd /mnt/disks/game-data
 backup_file=spigot-$(date +%Y%m%d-%H%M%S).tgz
-tar zcvf $backup_file spigot
+tar --exclude spigot/plugins/dynmap -zcvf $backup_file spigot
 gsutil cp $backup_file gs://minecraft-abekoh-backup/
 rm $backup_file
